186a47e50e9c28172fe4df46bee57b971e69091a
rajashrichonde <rajashrichonde123@gmail.com>
2022-11-21 18:04:32 +0530

    added config file

---
+++ config.py
@@ -0,0 +1,7 @@
+import os 
+    
+    
+HOST_NAME="127.0.0.1"
+PORT_NUMBER=5001
+MODEL_FILE_PATH=os.path.join("project_app","lr_model.pkl")
+JSON_FILE_PATH=os.path.join("project_app","target_names.json")
